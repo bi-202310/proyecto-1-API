c319965a884985dfcf505c05bdb03812d01f130d
est-gonzalezr <74991415+est-gonzalezr@users.noreply.github.com>
2023-04-19 11:41:11 -0500

    Make working changes

--- main.py
+++ main.py
@@ -14,47 +14,44 @@ nltk.download('punkt')
 
 app = FastAPI()
 
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
 
+
 @app.post("/predict/bow-rf")
 def predict_bow_rf(data: DataModel):
-    print(data)
-    d = data.dict()
-    print(d)
-    df = pd.DataFrame(d, columns = list(d.keys()), index=[0])
-    print(df)
-    
-    #df.columns = data.columns()
+    df = pd.Series(data.dict().values())
     model = load('assets/bow_rf.joblib')
     prediction = model.predict(df)
-    return prediction
+    return prediction[0]
+
 
 @app.post("/predict/tfidf-gb")
 def predict_tfidf_gb(data: DataModel):
-    df = pd.DataFrame(d, columns=list(d.keys()), index=[0])
-    print(df)
-    model = load('assets/tfidf-gb.joblib')
+    df = pd.Series(data.dict().values())
+    model = load('assets/tfidf_gb.joblib')
     prediction = model.predict(df)
-    return prediction
+    return prediction[0]
+
 
 @app.post("/predict/tfidf-rf")
 def predict_tfidf_rf(data: DataModel):
-    df = pd.DataFrame(data.dict(), columns = data.dict().keys(), index=[0])
-    df.columns = data.columns()
+    df = pd.Series(data.dict().values())
     model = load('assets/tfidf_rf.joblib')
     prediction = model.predict(df)
-    return prediction
+    return prediction[0]
+
 
 @app.post("/predict/tfidf-nb")
 def predict_tfidf_nb(data: DataModel):
-    df = pd.DataFrame(data.dict(), columns = data.dict().keys(), index=[0])
-    df.columns = data.columns()
+    df = pd.Series(data.dict().values())
     model = load('assets/tfidf_nb.joblib')
     prediction = model.predict(df)
-    return prediction
+    return prediction[0]
